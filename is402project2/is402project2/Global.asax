8d65df582aa5606d22318532a9acfac038b35f2d
Conner Olsen <cobackup123@gmail.com>
2017-12-12 17:39:47 -0700

    fsd

---
+++ is402project2/is402project2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="is402project2.MvcApplication" Language="C#" %>
---
+++ is402project2/is402project2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(is402project2.Startup))]
+namespace is402project2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
